a20682f40158629d7e8c661d747007ca7789c595
Dmitry Torba <dmitrytorba@gmail.com>
2021-12-22 10:59:14 -0800

    add location api

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ app = FastAPI()
 
 @app.get("/habitat/")
 async def root():
-    return {"message": "alive"}
+  return {"message": "alive"}
 
 # any timer started
 @app.post("/habitat/clockify/start")
-async def root():
-    return {"message": "started"}
+async def timeIn():
+  return {"message": "started"}
 
 # any timer stopped
 @app.post("/habitat/clockify/stop")
-async def root():
-    return {"message": "stopped"}
+async def timeOut():
+  return {"message": "stopped"}
+
+
+@app.get("/habitat/location/{device}/{loc}")
+async def location(device, loc):
+  msg = "located {} {}".format(device, loc)
+  print(msg)
+  return {"message": msg}
