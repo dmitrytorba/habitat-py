d3cfd60011057369f1b3162ff47a5555dadc3521
Dmitry Torba <dmitrytorba@gmail.com>
2021-12-22 08:03:06 -0800

    switch to post

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/habitat/")
 async def root():
     return {"message": "alive"}
 
 # any timer started
-@app.get("/habitat/clockify/start")
+@app.post("/habitat/clockify/start")
 async def root():
     return {"message": "started"}
 
 # any timer stopped
-@app.get("/habitat/clockify/stop")
+@app.post("/habitat/clockify/stop")
 async def root():
     return {"message": "stopped"}
