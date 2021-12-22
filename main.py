9a0a075b5ab590d6e911b3eeb2420be929f77ffd
Dmitry Torba <dmitrytorba@gmail.com>
2021-12-22 08:01:19 -0800

    debug root path

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI(root_path='/habitat')
+app = FastAPI()
 
 
-@app.get("/")
+@app.get("/habitat/")
 async def root():
     return {"message": "alive"}
 
 # any timer started
-@app.get("/clockify/start")
+@app.get("/habitat/clockify/start")
 async def root():
     return {"message": "started"}
 
 # any timer stopped
-@app.get("/clockify/stop")
+@app.get("/habitat/clockify/stop")
 async def root():
     return {"message": "stopped"}
