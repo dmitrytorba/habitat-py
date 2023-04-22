d8d36314a7b7b7276b52c84d76e5929bc940eae1
Dmitry Torba <dmitrytorba@gmail.com>
2023-04-22 09:15:51 -0700

    fix(schedule) time loop

--- onprem.py
+++ onprem.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import schedule
 from unifi import block_tablets, unblock_tablets
+import time
 
 load_dotenv()
 
@@ -18,3 +19,7 @@ schedule.every().day.at("07:00").do(unblock_tablets)
 schedule.every().day.at("08:25").do(block_tablets)
 schedule.every().day.at("12:30").do(unblock_tablets)
 schedule.every().day.at("22:45").do(block_tablets)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
